eb09da4bf9c88c45e6aeafa9e9cf421c26a96140
faizal121 <37105108+faizal121@users.noreply.github.com>
2018-03-06 18:38:09 +0700

    Add files via upload

---
+++ PhpProject1/index.php
@@ -0,0 +1,47 @@
+<html>
+    <head>
+        <title>Belajar tabel</title>    <!-- bagian untuk memberi judul -->
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    </head>
+    <body>  <!-- bagian body atau isi -->
+        <h1>Pemrograman WEB</h1> <!-- bagian untuk heading 1 -->
+        <div>Belajar WEB</div>
+        <center>
+            <h2>TABEL</h2>  <!-- bagian untuk heading 2 dan posisinya berada di tengah-->
+        </center>
+        <table style="border: 1px black solid " align="center" >  <!-- bagian untuk membuat dan mendesain warna tabel 
+                                                                        align=center digunakan agar tabelyang kita buat
+                                                                        menjadi rata tengah-->
+            <tr> <!-- bagian untuk mendefinisikan tabel row -->
+                <td style="border: 1px black solid;text-align: center" rowspan="2">NOMOR</td>
+                <td style="border: 1px black solid;text-align: center" rowspan="2">NAMA</td>
+                <td style="border: 1px black solid;text-align: center" colspan="8">BULAN</td>
+            </tr>
+            <tr>
+                <td style="border: 1px black solid;text-align: center">JAN</td> <!-- sedangkan td digunakan untuk tabel data -->
+                <td style="border: 1px black solid;text-align: center">FEB</td>
+                <td style="border: 1px black solid;text-align: center">MAR</td>
+                <td style="border: 1px black solid;text-align: center">APR</td>
+                <td style="border: 1px black solid;text-align: center">MEI</td>
+                <td style="border: 1px black solid;text-align: center">JUN</td>
+                <td style="border: 1px black solid;text-align: center">JUL</td>
+                <td style="border: 1px black solid;text-align: center">AGS</td>
+            </tr>
+            <tr>
+                <td style="border: 1px black solid;text-align: center">01</td>
+                <td style="border: 1px black solid;text-align: center">Faizal</td>
+                <td style="border: 1px black solid">B03 Kolom 3</td>
+                <td style="border: 1px black solid">B03 Kolom 4</td>
+                <td style="border: 1px black solid">B03 Kolom 5</td>
+                <td style="border: 1px black solid">B03 Kolom 6</td>
+                <td style="border: 1px black solid">B03 Kolom 7</td>
+                <td style="border: 1px black solid">B03 Kolom 8</td>
+                <td style="border: 1px black solid">B03 Kolom 9</td>
+                <td style="border: 1px black solid">B03 Kolom 10</td>
+            </tr>
+            
+            
+        </table>
+    </body>
+</html>
